d2b798f478239f984836f8ccc1700a6ab077b220
Joselio Jr. <joselio.fc@hotmail.com>
2025-09-09 21:04:55 -0300

    ✨ feat: Adição do primeiro exercício da Atividade 2
    Implementação de uma tela de Login, utilizando formulário.

---
+++ Angular/login/src/app/components/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  onLogin() {
+    if (this.email && this.password) {
+      console.log('Login realizado:', { email: this.email, password: this.password });
+    }
+  }
+}
